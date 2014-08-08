61c8e1cdbe34199d6c265ed3afc250b56b45f306
Michal Koutný <xm.koutny@gmail.com>
2014-08-08 17:43:03 +0200

    xcs-configure more robust

--- xcs-configure.sh
+++ xcs-configure.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 #
 # See: http://belion.tumblr.com/post/36151777927/ptam-compilation-notes
 #
 
+DIR="`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`"
 CXXFLAGS="-Wno-unused-variable"
-./configure --disable-widgets --prefix=$PWD/../../Build/gvars
+./configure --disable-widgets --prefix=$DIR/../../Build/gvars
